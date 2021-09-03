33811a2862aaa4142be4da735b6f7bb6de3af131
Kristina <rkcrisafi@gmail.com>
2021-09-03 00:58:50 -0400

    Add instructions to README and add comments

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # README
 
+Welcome to Shorty App! :)
+
 The URL shortener is written using the most familiar to me framework, Ruby on Rails.
 
-For custom Links, I chose to use only custom second halves since I am using only one domain. 
+To set it up, follow next steps:
+
+(make sure to install ruby ($ ruby -v) and rails ($ rails -v) beforehand)
+
+1. clone the repo
+2. install the dependencies ($ bundle install)
+3. run these commands:
+    $ rails db:create
+    $ rails db:migrate
+
+4. run the server ($ rails s)
+5. to test please use Rails console ($ rails c)
+6. to run tests ($ rails tests)
+
+
+I created 2 tables: urls and short_url_visits
+
+Urls table is where we persist our newly created urls (long and short).
+Short url visits is where a record is made every time request is being made to a short url. From this table, we are able to draw statistics as well.
+
+Urls controller handles creation of new short links (POST '/urls') and redirecting short links to original URLs (GET '/gi93N2').
+
+For custom Links, I chose to use only custom second halves since I am using only one domain.
+
+Short url visits controller is responsible for delivering the stat data to users.
+
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,25 +7,32 @@ class UrlsController < ApplicationController
             @url_visit = ShortUrlVisit.create(short_url: build_url(params[:path]))
             redirect_to @url.long_url
         else
+            # in case a short url does not exist
             render json: { message: 'Not found' }, status: 404
         end
     end
     
     def create
+        # for a custom back-half
         if url_params[:custom_back_half].present?
             short_url = build_url(url_params[:custom_back_half])
             url = Url.find_by(short_url: short_url)
+            # if it is been declared, we won't allow client to create a duplicate
             if  url.present?
                 render json: { message: "This URL has already been claimed for #{url.long_url}" }, status: 409
                 return
             end
         else
+            # for basis short links
             @url = url = Url.find_by(long_url: url_params[:long_url])
+            # render exisiting short link if long URL is already in the DB
             if @url.present?
                 render :show
                 return
             else
+                # generate random back-half
                 short_url = build_url(SecureRandom.alphanumeric(7))
+                # check for an extremely rare case of collision 
                 while Url.where(short_url: short_url).present?
                     short_url = build_url(SecureRandom.alphanumeric(7))
                 end
